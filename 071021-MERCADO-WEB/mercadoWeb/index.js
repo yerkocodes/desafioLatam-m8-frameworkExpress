c9d2c2e66cb8d22e228cb16b3c7fba5344f4e17f
Yerko Cortes Baeza <yerkodigo@gmail.com>
2021-10-18 07:55:02 -0300

    Added basic structure 'MercadoWeb'

---
+++ 071021-MERCADO-WEB/mercadoWeb/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const expressHandlebars = require('express-handlebars');
+const PORT = process.env.PORT || 3000;
+
+//Servidor a la escucha.
+app.listen(PORT, () => {
+  console.log('Listening server on port: ' + PORT);
+});
+
+//Definir handlebars como motor de vista.
+app.set('view engine', 'handlebars');
+
+//Configuración del motor.
+app.engine(
+  'handlebars',  // Extensión de los archivos.
+  expressHandlebars({
+    layoutsDir: __dirname + '/views', // Ruta de los layouts.
+    partialsDir: __dirname + '/views/componentes/',  // Ruta de los parciales.
+  })
+);
+
+//Middleware Bootstrap.
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
+//Middleware Jquery.
+//app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
+
+app.get('/', (req, res) => {
+  res.render('Inicio', {
+    layout: 'Inicio',
+  });
+});
