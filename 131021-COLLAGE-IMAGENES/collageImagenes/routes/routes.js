25d0e2f4c0a93a9a0f645a1f61185c3a514b0889
Yerko Cortes Baeza <yerkodigo@gmail.com>
2021-11-02 13:10:26 -0300

    'Collage de Imagenes' project ready

--- 131021-COLLAGE-IMAGENES/collageImagenes/routes/routes.js
+++ 131021-COLLAGE-IMAGENES/collageImagenes/routes/routes.js
@@ -20,6 +20,7 @@ router.post('/imagen', (req, res) => {
   const { posicion } = req.body;
   const rutaImg = path.join(__dirname, '../public/assets/imgs');
 
+  //console.log(posicion);
   imagen.mv(`${rutaImg}/imagen-${posicion}.jpg`, (err) => {
     err ? res.send(`Lo sentimos pero no se pudo ejecurar la peticion. ${err.message}.`) : res.redirect('/imagen');
   });
@@ -29,7 +30,8 @@ router.get('/deleteImg/:imagen', (req, res) => {
   const { imagen } = req.params;
   const rutaImg = path.join(__dirname, '../public/assets/imgs');
   unlink(`${rutaImg}/${imagen}`, (err) => {
-    err ? console.log('Ocurrio un error y no se pudo eliminar la foto.') : res.redirect('/imagen') ;
+    //err ? console.log('Ocurrio un error y no se pudo eliminar la foto.') : res.redirect('/imagen') ;
+    err ? res.send(`<h1>No se pudo eliminar la foto.</h1> <br> <button><a href="/">Volver a inicio</a></button>`) : res.redirect('/imagen') ;
   });
 });
 
